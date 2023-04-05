aa49e0fdde11f40e548dd158a3194da440c4cec3
hababa2 <zactanpeterson@gmail.com>
2023-04-05 13:03:27 -0600

    Isometric tilemap

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+	private new Rigidbody2D rigidbody;
 
+	private float MOVEMENT_SPEED = 2.0f;
 
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+		float inputX = Input.GetAxis("Horizontal") * MOVEMENT_SPEED * Time.deltaTime;
+		float inputY = Input.GetAxis("Vertical") * MOVEMENT_SPEED * Time.deltaTime;
+
+		transform.Translate(new Vector3(inputX, inputY, 0.0f));
     }
 }
