a7d089dcc57c2729dab514268c46729d189b393b
hababa2 <zactanpeterson@gmail.com>
2023-04-26 09:43:11 -0600

    Enemy Attacking

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,31 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+[RequireComponent(typeof(CharacterController))]
 public class EnemyController : MonoBehaviour
 {
 	[SerializeField] private RectTransform healthBar;
 	private Transform player;
+	private LayerMask attackMask;
+	private CharacterController controller;
 
-	public static readonly int MAX_HEALTH = 100;
+	private static readonly float MOVEMENT_SPEED = 4.0f;
+	private static readonly float ATTACK_COOLDOWN = 1.0f;
+	private static readonly int MAX_HEALTH = 100;
 
 	private int health = MAX_HEALTH;
+	private int damage = 20;
+	private float attacktimer = 0.0f;
 
 	private void Awake()
 	{
-		player = FindFirstObjectByType<PlayerController>().transform;
+		player = FindFirstObjectByType<PlayerController>().transform; 
+		controller = GetComponent<CharacterController>();
+		attackMask = LayerMask.GetMask("Player");
 	}
 
 	private void Update()
 	{
-		//TODO: Check for player and if they're in sight
-
-		Vector3 look = player.position - transform.position;
-
-		transform.eulerAngles = new Vector3(0.0f, Mathf.Rad2Deg * Mathf.Atan2(look.x, look.z), 0.0f);
-
 		if (health <= 0)
 		{
 			Destroy(gameObject);
+		}
+
+		attacktimer -= Time.deltaTime;
+
+		if (player != null)
+		{
+			Vector3 look = player.position - transform.position;
+
+			transform.eulerAngles = new Vector3(0.0f, Mathf.Rad2Deg * Mathf.Atan2(look.x, look.z), 0.0f);
+
+			//TODO: Check if enemy has line of sight to player
+			if (attacktimer <= 0.0f) { Attack(); }
 		}
 	}
 
@@ -39,6 +54,17 @@ public class EnemyController : MonoBehaviour
 
 	private void Attack()
 	{
+		attacktimer = ATTACK_COOLDOWN;
+		Quaternion rot = Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up);
 
+		Collider[] colliders = Physics.OverlapBox(transform.position + rot * Vector3.forward, new Vector3(0.75f, 0.75f, 0.45f), rot, attackMask.value);
+
+		foreach (Collider collider in colliders)
+		{
+			if (collider.gameObject.TryGetComponent(out PlayerController controller))
+			{
+				controller.Damage(damage);
+			}
+		}
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,19 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-	private LayerMask attackMask;
-
-	private float MOVEMENT_SPEED = 4.0f;
-	private int damage = 20;
-
+	[SerializeField] private RectTransform healthBar;
 	private CharacterController controller;
 	private Vector2 halfScreenSize;
+	private LayerMask attackMask;
+
+	private static readonly float MOVEMENT_SPEED = 4.0f;
+	private static readonly float ATTACK_COOLDOWN = 0.5f;
+	private static readonly float IFRAME_TIME = 0.25f;
+	private static readonly int MAX_HEALTH = 100;
+	private int health = MAX_HEALTH;
+	private int damage = 20;
+	private float attackTimer = 0.0f;
+	private float IframeTimer = 0.0f; //TODO: Hit/Iframe Effect
 
 	private void Start()
 	{
@@ -25,6 +31,8 @@ public class PlayerController : MonoBehaviour
 
 	private void Update()
 	{
+		if (health <= 0) { Destroy(gameObject); }
+
 		Vector2 mousePos = (Vector2)Input.mousePosition - halfScreenSize;
 
 		transform.eulerAngles = new Vector3(0.0f, Mathf.Rad2Deg * Mathf.Atan2(mousePos.x, mousePos.y) + 45.0f, 0.0f);
@@ -34,14 +42,15 @@ public class PlayerController : MonoBehaviour
 
 		controller.Move(Quaternion.AngleAxis(45.0f, Vector3.up) * new Vector3(inputX, 0.0f, inputY));
 
-		if(Input.GetMouseButtonDown(0))
-		{
-			Attack();
-		}
+		attackTimer -= Time.deltaTime;
+		IframeTimer -= Time.deltaTime;
+
+		if(Input.GetMouseButtonDown(0) && attackTimer <= 0) { Attack(); }
 	}
 
 	private void Attack()
 	{
+		attackTimer = ATTACK_COOLDOWN;
 		Quaternion rot = Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up);
 
 		Collider[] colliders = Physics.OverlapBox(transform.position + rot * Vector3.forward, new Vector3(0.75f, 0.75f, 0.45f), rot, attackMask.value);
@@ -52,6 +61,17 @@ public class PlayerController : MonoBehaviour
 			{
 				controller.Damage(damage);
 			}
+		}
+	}
+
+	public void Damage(int damage)
+	{
+		if (IframeTimer <= 0.0f)
+		{
+			health -= damage;
+			healthBar.sizeDelta = new Vector2((float)health / MAX_HEALTH * 500.0f, 60.0f);
+			IframeTimer = IFRAME_TIME;
+			Debug.Log("Hit");
 		}
 	}
 }
